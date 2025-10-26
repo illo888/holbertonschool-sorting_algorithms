f7b0b7e90894feac3067ec3f30c38eaff40d7c5d
root <root@ip-10-42-172-60.ec2.internal>
2025-10-26 13:04:57 +0000

    Implement quick sort with Lomuto partition
    - Use Lomuto partition scheme (pivot = last element)
    - Implement recursive sorting approach
    - Print array after each swap operation
    - Optimize by avoiding unnecessary swaps

---
+++ 3-quick_sort.c
@@ -0,0 +1,83 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two integers in an array
+ * @a: First integer
+ * @b: Second integer
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * lomuto_partition - Lomuto partition scheme
+ * @array: Array to partition
+ * @low: Starting index
+ * @high: Ending index
+ * @size: Size of the array
+ *
+ * Return: Index of the pivot after partitioning
+ */
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
+	if (i + 1 != high)
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+
+/**
+ * quick_sort_recursive - Recursive quick sort function
+ * @array: Array to sort
+ * @low: Starting index
+ * @high: Ending index
+ * @size: Size of the array
+ */
+void quick_sort_recursive(int *array, int low, int high, size_t size)
+{
+	int pivot_index;
+
+	if (low < high)
+	{
+		pivot_index = lomuto_partition(array, low, high, size);
+		quick_sort_recursive(array, low, pivot_index - 1, size);
+		quick_sort_recursive(array, pivot_index + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Sorts array using quick sort algorithm
+ * @array: Array of integers to sort
+ * @size: Size of the array
+ *
+ * Description: Implements quick sort with Lomuto partition scheme.
+ * Pivot is always the last element of partition.
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quick_sort_recursive(array, 0, size - 1, size);
+}
