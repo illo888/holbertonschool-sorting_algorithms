651a32b7ba69b668efe49693004f2a77c7dd85a9
root <root@ip-10-42-172-60.ec2.internal>
2025-10-26 12:46:34 +0000

    Implement bubble sort algorithm
    - Sort array in ascending order using bubble sort
    - Print array after each swap operation
    - Optimize with early termination if no swaps occur
    - Handle edge cases (NULL array, size < 2)

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order
+ * @array: Array of integers to sort
+ * @size: Size of the array
+ *
+ * Description: Implements bubble sort algorithm, printing array
+ * after each swap operation
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp, swapped;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+		if (swapped == 0)
+			break;
+	}
+}
